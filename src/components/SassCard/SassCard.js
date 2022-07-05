197f33cc8f6b85a11cab5bdd85a11089205e3a49
owen <owen.thiele@curtin.edu.au>
2022-07-05 18:11:42 +0800

    updated sass card

--- src/components/SassCard/SassCard.js
+++ src/components/SassCard/SassCard.js
@@ -5,11 +5,21 @@ export default function SassTest({id, selectedCard}) {
 
   return(
         <div className='container'>
-          <div className={`${id === selectedCard ? "face--front--flipped": "face--front"}`}>
-            <img src={logo} className="logo" alt="logo" />
+          
+          <div className={`${id === selectedCard ? "face--image--flipped": "face--image"}`}>
+
           </div>
+          
           <div className={`${id === selectedCard ? "face--back--flipped": "face--back"}`}>
             back 
           </div>
+
+          <div className={`${id === selectedCard ? "face--front--flipped": "face--front"}`}>
+            front
+          </div>
+
         </div>)
 }
+
+{/* <img src={logo} className="logo" alt="logo" /> */}
+            {/* <div className='image'><div className="face--front--content"><p>text text text </p><p>text text text </p></div></div> */}
