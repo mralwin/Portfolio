241329b0f9deecef221e877d83b460d30535d993
owen <owen.thiele@curtin.edu.au>
2022-07-09 19:19:45 +0800

    added new card structure

--- src/components/SassCard/SassCard.js
+++ src/components/SassCard/SassCard.js
@@ -5,11 +5,18 @@ export default function SassTest({id, selectedCard}) {
 
   return(
         <div className='container'>
-          
-          <div className={`${id === selectedCard ? "face--image--flipped": "face--image"}`}>
-
+          <div className='front'>
+            <div className='image'></div>
+            <div className='inner'></div>
           </div>
-          
+          <div className='back'>
+            <div className='inner'></div>
+          </div>
+
+
+
+
+          <div className={`${id === selectedCard ? "face--image--flipped": "face--image"}`}></div>
           <div className={`${id === selectedCard ? "face--back--flipped": "face--back"}`}>
             back 
           </div>
